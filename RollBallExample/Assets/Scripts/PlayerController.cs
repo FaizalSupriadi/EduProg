9baa79530213fc9ea313bc75f0c2c95382b2e96c
FaizalSupriadi <faizal.faizalrachmansupriadi@student.hu.nl>
2021-09-19 16:08:35 +0200

    Finished

--- RollBallExample/Assets/Scripts/PlayerController.cs
+++ RollBallExample/Assets/Scripts/PlayerController.cs
@@ -1,25 +1,49 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerController : MonoBehaviour
 {
-	private Rigidbody rb;
-	public float speed;
-    // Start is called before the first frame update
-    void Start()
+    public float speed;
+    public Text countText;
+    public Text winText;
+
+    private Rigidbody rb;
+    private int count;
+
+    void Start ()
     {
         rb = GetComponent<Rigidbody>();
+        count = 0;
+        SetCountText();
+        winText.text = "";
     }
 
-    // Update is called once per frame
-    void FixedUpdate()
+    void FixedUpdate ()
     {
-        float moveHorizontal = Input.GetAxis("Horizontal");
-        float moveVertical = Input.GetAxis("Vertical");
+        float moveHorizontal = Input.GetAxis ("Horizontal");
+        float moveVertical = Input.GetAxis ("Vertical");
 
-        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+        Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
 
-        rb.AddForce(movement * speed);
+        rb.AddForce (movement * speed);
+    }
+
+    void OnTriggerEnter(Collider other) 
+    {
+        if (other.gameObject.CompareTag ("Money"))
+        {
+            other.gameObject.SetActive (false);
+            count += 1;
+            SetCountText();
+        }
+    }
+
+    void SetCountText(){
+    	countText.text = "Count: " + count.ToString();
+    	if(count >= 11){
+    		winText.text = "You Win!";
+    	}
     }
 }
